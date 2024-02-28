013c3cdc41192513fb6342d88c5a0ecb9bd2a443
csonic235 <csonic235@gmail.com>
2024-02-28 23:53:20 +1300

    qol changes

--- bot.py
+++ bot.py
@@ -41,20 +41,22 @@ def predict_emoji(text, classifier, vectorizer, tfidf_transformer, threshold=0.1
 
 def answer_question(message):
     #first check if message is a question directed at lisabot
-    if message.strip().endswith('?') and len(message) > 1:
+    if message.strip().endswith('?'):
+        if len(message) > 1:
+            likert_scale = {
+            "strong_agree": ["YES", "YESSSS", "Absolutely!"],
+            "agree": ["yes", "yessir", "ya"],
+            "neutral": ["I actually dont know", "not rn", "lol", "I wish I could but I don't want to"],
+            "disagree": ["nope", "ewww no", "naurrr"],
+            "strong_disagree": ["WHAT NO", "NO", "Absolutely not!!!"]
+            }
 
-        likert_scale = {
-        "strong_agree": ["YES", "YESSSS", "Absolutely!"],
-        "agree": ["yes", "yessir", "ya"],
-        "neutral": ["I actually dont know", "not rn", "lol", "I wish I could but I don't want to"],
-        "disagree": ["nope", "ewww no", "naurrr"],
-        "strong_disagree": ["WHAT NO", "NO", "Absolutely not!!!"]
-        }
+            random_opinion = random.choice(list(likert_scale.keys()))
+            likert_answer = random.choice(likert_scale[random_opinion])
 
-        random_opinion = random.choice(list(likert_scale.keys()))
-        likert_answer = random.choice(likert_scale[random_opinion])
-
-        return likert_answer
+            return likert_answer
+        else:
+            return "?"
 
 # put canned responses
 def response_to(message):
@@ -65,6 +67,7 @@ def response_to(message):
         "lisa burger":  lambda: "https://media.discordapp.net/attachments/1113266262345273428/1187365137598922802/imageedit_9_9053779888.png?ex=65969ef4&is=658429f4&hm=8ea4ed39282ce942d2556cced752afcc23353607d02a60e7b5119a4ac9c8e43f&=&format=webp&quality=lossless&width=462&height=462",
         "opinion":      lambda: "https://cdn.discordapp.com/attachments/1168400523104358442/1193073645715734578/y147cc9pwqac1.png?ex=65ab636b&is=6598ee6b&hm=c081a2716fbb8415c6a8be0d8ad8fa24fbfa639a5d97d0d673b15c65e2b984ca&",
         "research":     lambda: "https://cdn.discordapp.com/attachments/1205091789195837502/1205099075494739978/image.png?ex=65d722f9&is=65c4adf9&hm=0d250958d796faf568f638b7fa9a260afeef7b439f9fafc4a02f3d0b776728af&",
+        "are you the real lisa?": lambda: "I am the real Lisa :lisaCat:",
         # add here as required
     }
 
@@ -159,7 +162,7 @@ async def on_message_create(event: MessageCreate):
             if random.randint(1, 2) == 1:
                 await msg.reply(likert_answer + emojis_to_send)
             else:
-                await msg.channel.send(likert_answer + "" + emojis_to_send)
+                await msg.channel.send(likert_answer + " " + emojis_to_send)
         else:
             if random.randint(1, 2) == 1:
                 await msg.channel.send("HUH")
